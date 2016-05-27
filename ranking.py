0999c24ca82a85bf8306e717e51505ff1e0dc36e
Francesco Siani <francesco.siani.cs@gmail.com>
2016-05-27 14:44:09 +0200

    Defined SpamMass

--- ranking.py
+++ ranking.py
@@ -8,28 +8,28 @@ def pageRank(graph, s, step, confidence):
 
 	# Initialization
 	rank = dict()
-	for i in nodes:
-		rank[i] = float(1)/2
+	for node in nodes:
+		rank[node] = float(1)/2
 
 
 	tmp = dict()
 	while not done and time < step:
 		time += 1
 
-		for i in nodes:
+		for node in nodes:
 			# Each node receives a share of 1/n with probability 1-s
-			tmp[i] = float(1-s)/n 
+			tmp[node] = float(1-s)/n 
 
-		for i in nodes:
-			for j in graph[i]:
-				# Each node receives a fraction of its neighbor rank with probability s
-				tmp[j] += float(s*rank[i])/len(graph[i])
+		for node in nodes:
+			for neighbour in graph[node]:
+				# Each node receives a fraction of its neighbour rank with probability s
+				tmp[neighbour] += float(s*rank[node])/len(graph[node])
 
 		# Computes the distance between the old rank vector and the new rank vector in L_1 norm
 		diff = 0
-		for i in nodes:
-			diff += abs(rank[i] - tmp[i])
-			rank[i] = tmp[i]
+		for node in nodes:
+			diff += abs(rank[node] - tmp[node])
+			rank[node] = tmp[node]
 
 		if diff <= confidence:
 			done = 1
@@ -50,7 +50,7 @@ def pageRank(graph, s, step, confidence):
 #	PageRank near the top of the list
 # 2 Pick a domain whose membership is controlled, on the assumption that it is hard
 #	for a spammer to get their pages into these domains (e.g. .edu, .gov, .mil)
-def trustRank():
+def trustRank(graph):
 	return
 
 
@@ -61,8 +61,16 @@ def trustRank():
 # (r-t)/r. 
 # A negative or small positive spam mass means that p is probably not a spam page,
 # while a spam mass close to 1 suggests that the page probably is spam.
-def spamMass():
-	return
+def spamMass(graph):
+	prTime, prRank = pageRank(graph,0.85,75,0)
+	trTime, trRank = trustRank(graph)
+
+	finalRank = dict()
+	for node in graph.keys():
+		finalRank[node] = ( prRank[node] - trRank[node] ) / trRank[node] 
+
+
+	return finalRank
 
 
 
