9335b66ec4896a570d144be4e1f3eb0fe03714f4
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-06-16 20:02:01 +0200

    Fixed crawler

--- PageRanking/spider.py
+++ PageRanking/spider.py
@@ -31,44 +31,51 @@ def crawl(url, number_of_links_to_follow, graph = None, verbose= False, graphVer
         for actual_url in actual_layer:
             if verbose:
                 print("Examining: " + actual_url)
-            if actual_url not in graph:
-                graph[actual_url] = set()
-                if graphVerbose:
-                    print("Added node: " + actual_url)
-            url_html = urllib.request.urlopen(actual_url)
-            # Pattern to check proper link
-            dom = lxml.html.fromstring(lxml.html.tostring(lxml.html.parse(url_html)))
-            for link in dom.xpath("//a/@href"):
-                if verbose:
-                    print("Link founded: " + link)
-                if linkPattern.match(link): 
-                    try:
-                        urllib.request.urlopen(link)
-                        if link not in graph:
-                            if graphVerbose:
-                                print("Adding node: " + link)
-                            graph[link] = set()
-                        graph[actual_url] = str(link)
-                        if graphVerbose:
-                            print("Adding edge from " + actual_url + " to " + link)
-                        if(link not in crawledLinks):
-                            if verbose:
-                                print("Adding " + link + " ")
-                            crawledLinks.add(link) 
-                            next_layer.add(link)
-                            i += 1
-                        else:
-                            if verbose:
-                                print("Refusing " + link + " because it was already added")
-                    except urllib.error.HTTPError as e:
-                        if errorVerbose:
-                            print(str(link) + " caused " +  str(e) + " so it wasn't added")
-                    except urllib.error.URLError as e:
-                        if errorVerbose:
-                            print(str(link) + " caused " + str(e) + " so it wasn't added")
-                else:
+            try:
+                url_html = urllib.request.urlopen(actual_url)
+                if actual_url not in graph:
+                    graph[actual_url] = set()
+                    if graphVerbose:
+                        print("Added node: " + actual_url)
+                # Pattern to check proper link
+                dom = lxml.html.fromstring(lxml.html.tostring(lxml.html.parse(url_html)))
+                for link in dom.xpath("//a/@href"):
                     if verbose:
-                        print("Refusing " + link + "because did not match regexp")
+                        print("Link founded: " + link)
+                    if linkPattern.match(link): 
+                        try:
+                            urllib.request.urlopen(link)
+                            if link not in graph:
+                                if graphVerbose:
+                                    print("Adding node: " + link)
+                                graph[link] = set()
+                            graph[actual_url].add(link)
+                            if graphVerbose:
+                                print("Adding edge from " + actual_url + " to " + link)
+                            if(link not in crawledLinks):
+                                if verbose:
+                                    print("Adding " + link + " ")
+                                crawledLinks.add(link) 
+                                next_layer.add(link)
+                                i += 1
+                            else:
+                                if verbose:
+                                    print("Refusing " + link + " because it was already added")
+                        except urllib.error.HTTPError as e:
+                            if errorVerbose:
+                                print(str(link) + " caused " +  str(e) + " so it wasn't added")
+                        except urllib.error.URLError as e:
+                            if errorVerbose:
+                                print(str(link) + " caused " + str(e) + " so it wasn't added")
+                    else:
+                        if verbose:
+                            print("Refusing " + link + "because did not match regexp")
+            except urllib.error.HTTPError as e:
+                if errorVerbose:
+                    print("Start link " + str(actual_url) + " caused " +  str(e) + " so it wasn't added")
+            except urllib.error.URLError as e:
+                if errorVerbose:
+                    print("Start link " + str(actual_url) + " caused " +  str(e) + " so it wasn't added")
         if verbose:
             print("-----------------------------")
             print("Finished layer " + str(layer))
