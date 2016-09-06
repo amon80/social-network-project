e8d2d08f6af541f035f7baf9f1fec8004b36aafe
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-09-06 15:14:45 +0200

    Fixed graph reading and added function to normalize graph

--- PageRanking/graph.py
+++ PageRanking/graph.py
@@ -1,8 +1,16 @@
+import sys
+
 def write_graph(graph, output_file):
     with open(output_file, "w") as f:
         for node in graph:
             for edge in graph[node]:
-                f.write(node + " " + edge + "\n")
+                f.write(str(node) + " " + str(edge) + "\n")
+
+def write_graph_as_csv(graph, output_file):
+    with open(output_file, "w") as f:
+        for node in graph:
+            for edge in graph[node]:
+                f.write(str(node) + "," + str(edge) + "\n")
 
 def read_graph(input_file):
     graph = dict()
@@ -12,10 +20,10 @@ def read_graph(input_file):
             node1 = nodes[0]
             node2 = nodes[1]
             if node1 not in graph:
-                graph[node1] = list()
+                graph[node1] = set()
             if node2 not in graph:
-                graph[node2] = list()
-            graph[node1].append(node2)
+                graph[node2] = set()
+            graph[node1].add(node2)
     return graph
 
 def write_index(index, output_file):
@@ -27,3 +35,25 @@ def write_index(index, output_file):
                 for i in range(num_times):
                     f.write(query_term + ",")
             f.write("\n")
+
+
+def normalize_graph(input_file):
+    graph = read_graph(input_file)
+    graph_normalized = dict()
+    index_node_mapping = dict()
+    index = 1
+    for node in graph.keys():
+        index_node_mapping[node] = index
+        graph_normalized[index] = set()
+        index += 1
+    for node in graph.keys():
+        source_index = index_node_mapping[node]
+        for edge in graph[node]:
+            target_index = index_node_mapping[edge]
+            graph_normalized[source_index].add(target_index)
+
+    write_graph_as_csv(graph_normalized, input_file+'_normalized')
+
+
+if __name__ == "__main__":
+    normalize_graph(sys.argv[1])
