44207cd1695b6c469b525daee3ba1dff8cc7c279
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-09-07 10:20:15 +0200

    Added clean_graph module

---
+++ PageRanking/clean_graph.py
@@ -0,0 +1,43 @@
+from graph import read_graph, write_graph
+from generate_database import write_index
+from matching import create_indeces
+import sys
+
+def clean_graph(graph_input_file, nodes_to_be_removed_file):
+    graph = read_graph(graph_input_file)
+    with open(nodes_to_be_removed_file, 'r') as f:
+        for node_to_remove in f:
+            node_to_remove = node_to_remove.rstrip()
+            del graph[node_to_remove]
+            for node in graph:
+                if node_to_remove in graph[node]:
+                    graph[node].remove(node_to_remove)
+    write_graph(graph, graph_input_file+'_with_nodes_removed')
+
+
+def clean_graph_and_index(graph_input_file, index_input_file):
+    graph = read_graph(graph_input_file)
+    (index, inverted_index) = create_indeces(index_input_file)
+    page_to_remove = []
+    for page in index.keys():
+        if page not in graph.keys():
+            page_to_remove.append(page)
+    for page in page_to_remove:
+        del index[page]
+
+    node_to_remove = []
+    for node in graph.keys():
+        if node not in index.keys():
+            node_to_remove.append(node)
+
+    for node in node_to_remove:
+        del graph[node]
+        for node1 in graph:
+            if node in graph[node1]:
+                graph[node1].remove(node)
+
+    write_index(index, index_input_file+'_cleaned')
+    write_graph(graph, graph_input_file+'_cleaned')
+
+if __name__ == "__main__":
+    clean_graph_and_index(sys.argv[1], sys.argv[2])
