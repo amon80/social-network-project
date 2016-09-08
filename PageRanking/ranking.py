4691af4e4c0b5d2cb4e800531a81ec793f0e5970
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-09-09 00:27:44 +0200

    Optimized pagerank and finally fixed (hope definitively) trustrank

--- PageRanking/ranking.py
+++ PageRanking/ranking.py
@@ -6,25 +6,29 @@ from math import sqrt
 #matricial_pagerank works only on normlized graph given as transition matrix
 #for inverse pagerank give the function the inverse transition matrix
 #if given, tax must be a vector that sums to one(biased pagerank)
-def matricial_pageRank(transition_matrix, s=0.85, step=1000, confidence=0, tax = None, verbose = True):
+def matricial_pageRank(transition_matrix, s=0.85, step=1000, confidence=0, tax = None, rank = None, verbose = True):
     n = len(transition_matrix)
     nodes = range(n)
     done = False
     time = 0
 
-    rank = []
-    if tax == None:
+    if rank is None:
+        rank = []
+        for i in nodes:
+            rank.append(1/n)
+    if tax is None:
         tax = []
         for i in nodes:
             tax.append(1/n)
-    for i in nodes:
-        rank.append(1/n)
 
+    first_term = multiply(s,transition_matrix)
+    second_term = multiply((1-s),tax)
     while not done and time < step:
         time += 1 
         if verbose:
             print(time)
-        tmp = add(dot(multiply(s,transition_matrix), rank), multiply((1-s), tax))
+        first_term_bis = dot(first_term, rank)
+        tmp = add(first_term_bis, second_term)
         diff = 0
         tmp = tmp.A1
         for i in nodes:
@@ -34,6 +38,20 @@ def matricial_pageRank(transition_matrix, s=0.85, step=1000, confidence=0, tax =
         if diff <= confidence:
             done = True
     return time, rank
+
+#As above
+def matricial_trustRank(transition_matrix, trusted_pages, s=0.85, step = 1000, confidence = 0, verbose = True):
+    tax_vector = list()
+    n = len(transition_matrix)
+    for i in range(n):
+        tax_vector.append(0)
+    for page in trusted_pages:
+        tax_vector[page] = 1
+    rank_vector = list(tax_vector)
+    tax_vector = multiply((1/len(trusted_pages)),tax_vector)
+    time, rank = matricial_pageRank(transition_matrix, tax = tax_vector, rank = rank_vector, verbose = verbose, confidence = confidence, s = s, step = step)
+    return time,rank
+
 
 def pageRank(graph, s=0.85, step=1000, confidence=0, verbose = True):
     nodes = graph.keys()
@@ -72,69 +90,6 @@ def pageRank(graph, s=0.85, step=1000, confidence=0, verbose = True):
             done = True
     return time, rank
 
-
-# TrustRank is topic-sensitive PageRank, where the "topic" is a set of pages
-# believed to be trustworthy (not spam). The theory is that while a spam page
-# might easly be made to link to a trustworthy page, it is unlikely that a 
-# trustworthy page would link to a spam page.
-# The borderline area is a site with blogs or other opportunities for spammers
-# to create links.
-# Two approaches to develop a suitable teleport set of trustworthy pages:
-# 1 Let humans examine a set of pages and decide which of them are trustworthy.
-# 	For example, we might pick the pages of highest PageRank to examine,
-#	on the theory that, while link spam can raise a page's rank from the bottom
-#	to the middle of the pack, it is esssentially impossible to give a spam page a
-#	PageRank near the top of the list
-# 2 Pick a domain whose membership is controlled, on the assumption that it is hard
-#	for a spammer to get their pages into these domains (e.g. .edu, .gov, .mil)
-
-def trustRank(graph, trusted_pages, s=0.85, step=1000, confidence=0, verbose = True):
-    nodes = graph.keys()
-    n = len(nodes)
-    time = 0
-
-    # Initialization
-    rank = dict()
-
-    trusted_pages_set = set(trusted_pages)
-
-    for node in nodes:
-        if node not in trusted_pages_set:
-            # Each node receives a share of 1/n with probability 1-s
-            rank[node] = float(1)/n 
-
-    for node in trusted_pages:
-        rank[node] = float(1)
-
-    tmp = dict()
-    done = False
-
-    while not done and time < step:
-        time += 1
-        if verbose:
-            print(time)
-
-        # for node in trusted_pages:
-        #     tmp[node] = float(1)/2
-
-        for node in nodes:
-            # Each node receives a share of 1/n with probability 1-s
-            tmp[node] = float(1-s)/n 
-
-        for node in nodes:
-            for neighbour in graph[node]:
-                # Each node receives a fraction of its neighbour rank with probability s
-                tmp[neighbour] += float(s*rank[node])/len(graph[node])
-
-        # Computes the distance between the old rank vector and the new rank vector in L_1 norm
-        diff = 0
-        for node in nodes:
-            diff += abs(rank[node] - tmp[node])
-            rank[node] = tmp[node]
-
-        if diff <= confidence:
-            done = True
-    return time, rank
 
 # The idea behind spam mass is that we measure for each page the fraction
 # of its PageRank that comes from spam. We do so by computing both the 
