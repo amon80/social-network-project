a551d4c92aa7442cd22d3e04cdeeb1ca79fa4c46
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-09-10 11:13:39 +0200

    Fixed search methods

--- PageRanking/search.py
+++ PageRanking/search.py
@@ -1,16 +1,82 @@
-def query_database(query, graph, ranks, index, inverted_index, matching_function, alpha = 1, beta = 2):
+from matching import best_match, best_match2, sort_inverted_index
+from index import read_index
+from ranking import read_ranking
+from sys import argv
+
+def query_database_pagerank_bestmatch(query, pageranks, index, inverted_index):
     final_scores = dict()
     best_docs_matching_scores = dict()
     best_docs_rank_scores = dict()
 
-    best_docs_as_list_of_tuples = matching_function(query, inverted_index, index) 
+    #computing matching score
+    best_docs_as_list_of_tuples = best_match(query, inverted_index, index) 
     for best_doc_tuple in best_docs_as_list_of_tuples:
         best_docs_matching_scores[best_doc_tuple[0]] = best_doc_tuple[1]
-
-    for doc in best_docs:
-        best_docs_rank_scores[doc] = ranks[doc]
     
+    #combining the two scores
     for doc in best_docs:
-        final_scores[doc] = alpha * best_docs_matching_scores[doc] + beta * best_docs_rank_scores[doc]
+        final_scores[doc] = best_docs_matching_scores[doc] * pageranks[doc]
 
     return sorted(final_scores.items(), key=final_scores.get, reverse=True)
+
+#inverted_index must be given already sorted
+def query_database_pagerank_bestmatch2(query, pageranks, index, sorted_inverted_index):
+    final_scores = dict()
+    best_docs_matching_scores = dict()
+    best_docs_rank_scores = dict()
+
+    #computing matching score
+    best_docs_as_list_of_tuples = best_match2(query, sorted_inverted_index, index) 
+    for best_doc_tuple in best_docs_as_list_of_tuples:
+        best_docs_matching_scores[best_doc_tuple[0]] = best_doc_tuple[1]
+    
+    #combining the two scores
+    for doc in best_docs:
+        final_scores[doc] = best_docs_matching_scores[doc] * pageranks[doc]
+
+    return sorted(final_scores.items(), key=final_scores.__getitem__, reverse=True)
+
+def query_database_spammass_bestmatch(query, pageranks, spammass_ranks, index, inverted_index, spammass_threshold = 0.70):
+    final_scores = dict()
+    best_docs_matching_scores = dict()
+
+    #computing matching score
+    best_docs_as_list_of_tuples = best_match(query, inverted_index, index) 
+    for best_doc_tuple in best_docs_as_list_of_tuples:
+        best_docs_matching_scores[best_doc_tuple[0]] = best_doc_tuple[1]
+    
+    #combining the two scores
+    for doc in best_docs:
+        if spammass_ranks[doc] > spammass_threshold:
+            final_scores[doc] = best_docs_matching_scores[doc] * pageranks[doc]
+        else:
+            final_scores[doc] = 0
+
+    return sorted(final_scores.items(), key=final_scores.__getitem__, reverse=True)
+
+#inverted_index must be given already sorted
+def query_database_spammass_bestmatch2(query, pagerank, spammass_ranks, index, sorted_inverted_index, spammass_threshold = 0.70):
+    final_scores = dict()
+    best_docs_matching_scores = dict()
+    best_docs_rank_scores = dict()
+
+    #computing matching score
+    best_docs_as_list_of_tuples = best_match2(query, sorted_inverted_index, index) 
+    for best_doc_tuple in best_docs_as_list_of_tuples:
+        best_docs_matching_scores[best_doc_tuple[0]] = best_doc_tuple[1]
+    
+    #combining the two scores
+    #since spammass >= to 1 when its spam, the scores remains the same. If spammass is little or negative, score is boosted
+    for doc in best_docs:
+        if spammass_ranks[doc] > spammass_threshold:
+            final_scores[doc] = best_docs_matching_scores[doc] * pageranks[doc]
+        else:
+            final_scores[doc] = 0
+
+    return sorted(final_scores.items(), key=final_scores.get, reverse=True)
+
+if __name__ == "__main__":
+    query = ""
+    for i in range(1, len(argv)):
+        query += argv[i] + " "
+
