4ac23f9a1b53c832339eb1b356bebaa58bb9bd0b
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-09-11 15:31:19 +0200

    Made code more readable

--- PageRanking/clean_graph.py
+++ PageRanking/clean_graph.py
@@ -3,21 +3,23 @@ from index import read_index, write_index
 import sys
 
 #Use just this function
+#Maybe must be called more than once
 def clean_graph_and_index(graph_input_file, index_input_file):
     graph = read_graph(graph_input_file)
-    (index, inverted_index) = read_index(index_input_file)
+    index = read_index(index_input_file, integer = False, also_inverted = False)
     page_to_remove = []
-    #check for pages present in index but not in graph
-    for page in index.keys():
-        if page not in graph.keys():
+    
+    #check for pages present in index but not in graph, if there are, remove those pages from the index
+    for page in index:
+        if page not in graph:
             page_to_remove.append(page)
     for page in page_to_remove:
         del index[page]
 
-    #check for pages present in graph but not in index
+    #check for pages present in graph but not in index, if there are, remove those pages from the graph
     node_to_remove = []
-    for node in graph.keys():
-        if node not in index.keys():
+    for node in graph:
+        if node not in index:
             node_to_remove.append(node)
 
     for node in node_to_remove:
