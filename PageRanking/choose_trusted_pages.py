d40dca80afdeef9b39e02c30e2407eb44ddd9fbf
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-09-12 00:58:41 +0200

    Transformed into module

--- PageRanking/choose_trusted_pages.py
+++ PageRanking/choose_trusted_pages.py
@@ -2,18 +2,23 @@ from ranking import read_rankings, order_nodes
 from normalize import read_normalization_map
 from sys import argv
 
+def choose_trusted_pages(PageRanks, inv_PageRanks, num_pages):
+    nodes = range(len(PageRanks))
+    ordered_nodes_inv = order_nodes(nodes, inv_PageRanks)
+    ordered_nodes = order_nodes(nodes, PageRanks)
+    trusted_pages = ordered_nodes_inv[:num_pages] + ordered_nodes[:num_pages]
+    #Fortunately, target page is the only one on which the oracle says no
+    trusted_pages.remove(12639)
+    return trusted_pages
+
+def write_trusted_pages(trusted_pages_list, output_file_name):
+    with open(output_file_name, 'w') as f:
+        for page in trusted_pages_list:
+            f.write(str(page)+'\n')
+
 if __name__ == "__main__":
     prRank = read_rankings(argv[1])
     inv_prRank = read_rankings(argv[2])
     num_pages = int(argv[3])
-    nodes = range(len(prRank))
-    ordered_nodes_inv = order_nodes(nodes, inv_prRank)
-    ordered_nodes = order_nodes(nodes, prRank)
-    pages_on_which_use_oracle = ordered_nodes_inv[:num_pages] + ordered_nodes[:num_pages]
-    norm_map, inv_norm_map = read_normalization_map('mapping')
-    print("Inverted Pageranks")
-    for page in ordered_nodes_inv[:num_pages]:
-        print(inv_norm_map[page] + " - " + str(page))
-    print("Pageranks")
-    for page in ordered_nodes[:num_pages]:
-        print(inv_norm_map[page] + " - " + str(page))
+    trusted_pages = choose_trusted_pages(prRank, inv_prRank, num_pages)
+    write_trusted_pages(trusted_pages, 'trusted_pages_'+str(num_pages*2))
